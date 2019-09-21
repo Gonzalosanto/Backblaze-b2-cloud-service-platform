cf7d5c78a8740959ef4b9e3fdc4d322840d39df0
Grey Uzcategui <grey.uzcategui@sondeos.com.ar>
2019-09-20 23:13:43 -0300

    Identando y haciendo limpieza del proyecto

--- application/dataAccessObjects/Download.php
+++ application/dataAccessObjects/Download.php
@@ -1,25 +1,22 @@
-<?php 
+<?php
+
 //session_start();
 require_once('Request.php');
 require_once('DownloadAuth.php');
-  Request();
-    
-    //global $data;
-    
-    global $authToken;
-    global $fileName;
-      
-    //downloadAUTH
- // DownloadAuth();
-    
+Request();
 
+//global $data;
+
+global $authToken;
+global $fileName;
+
+//downloadAUTH
+// DownloadAuth();
 //Publico
 $download_url = "https://f000.backblazeb2.com"; // From b2_authorize_account call
 $bucket_name = "archivo1992";  // The NAME of the bucket you want to download from
 $file_name = "$fileName"; // The name of the file you want to download
 $uri = $download_url . "/file/" . $bucket_name . "/" . $file_name;
-
-
 
 $session = curl_init($uri);
 
@@ -27,11 +24,9 @@ curl_setopt($session, CURLOPT_HTTPGET, true); // HTTP GET
 curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);  // Receive server response
 $server_output = curl_exec($session); // Let's do this!
-curl_close ($session); // Clean up
+curl_close($session); // Clean up
 //echo ($server_output); // Tell me about the rabbits, George!*/
-
 // You will need to use the account authorization token if your bucket's type is allPrivate.
-
 //Privado
 
 $download_url = "https://f000.backblazeb2.com"; // From b2_authorize_account call
@@ -45,18 +40,17 @@ $session = curl_init($uri);
 // Add headers
 $headers = array();
 $headers[] = "Authorization: " . $auth_token;
-curl_setopt($session, CURLOPT_HTTPHEADER, $headers); 
+curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
 
 curl_setopt($session, CURLOPT_HTTPGET, true); // HTTP POST
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);  // Receive server response
 $server_output = curl_exec($session); // Let's do this!
-curl_close ($session); // Clean up
+curl_close($session); // Clean up
 //echo ($server_output); // Tell me about the rabbits, George!
 
-  header('Content-type:application/octet-stream'); //Acá le cambias el tipo de archivo (MimeType) por lo que quieras
-  header("Content-Transfer-Encoding: Binary");
-  header('Content-Disposition: attachment; filename ='.$file_name); //renombramos la descarga
-  echo($server_output);
-  exit();
+header('Content-type:application/octet-stream'); //Acá le cambias el tipo de archivo (MimeType) por lo que quieras
+header("Content-Transfer-Encoding: Binary");
+header('Content-Disposition: attachment; filename =' . $file_name); //renombramos la descarga
+echo($server_output);
+exit();
 
-        
--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,52 +1,48 @@
 <html lang="en"><head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Login</title>
 
-    
+        <!-- Bootstrap core CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+        <style>
+            .bd-placeholder-img {
+                font-size: 1.125rem;
+                text-anchor: middle;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                -ms-user-select: none;
+                user-select: none;
+            }
 
+            @media (min-width: 768px) {
+                .bd-placeholder-img-lg {
+                    font-size: 3.5rem;
+                }
+            }
+        </style>
+        <!-- Custom styles for this template -->
+        <!--<link href="signin.css" rel="stylesheet">-->
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/signin.css"); ?>">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
+    </head>
+    <body class="text-center">
+        <form class="form-signin" action="<?php echo base_url('index.php/home'); ?>" method= "post">
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
-    <!--<link href="signin.css" rel="stylesheet">-->
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/signin.css"); ?>">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input type="name" id="inputUser" name="inputUser" class="form-control" placeholder="Email address" required="" autofocus="">
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 
-
-  </head>
-  <body class="text-center">
-    <form class="form-signin" action="<?php echo base_url('index.php/home');?>" method= "post">
-  
-  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-  <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="name" id="inputUser" name="inputUser" class="form-control" placeholder="Email address" required="" autofocus="">
-  <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me"> Remember me
-    </label>
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  
-</form>
-</body>
+        </form>
+    </body>
 
 </html>
