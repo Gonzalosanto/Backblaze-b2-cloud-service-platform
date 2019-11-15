6898d8a15835f04d7a54498e06ebd6f665888df2
Grey Uzcategui <grey.uzcategui@sondeos.com.ar>
2019-11-14 23:42:34 -0300

    Agregando subida de archivos a backblaze

--- application/controllers/ServiceUploadController.php
+++ application/controllers/ServiceUploadController.php
@@ -4,45 +4,60 @@ class ServiceUploadController extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->helper('url');
+//        $this->load->helper('url');
+        $this->load->model(array('subida_php_backblaze_model'));
     }
 
     public function uploadFile() {
-        
-        $solicitarUrlFile= $this->solicitarUrlFile();
 
-        $file_name = "deftones.mp3";
-        $my_file = "/var/www/html/PlataformaWEB/assets/" . $file_name;
-        $handle = fopen($my_file, 'r');
-        $read_file = fread($handle, filesize($my_file));
-        $content_type = mime_content_type($my_file);
-        $sha1_of_file_data = sha1_file($my_file);
+        $archivo_subir = $this->subida_php_backblaze_model->listar_archivos_a_subir();
 
-        $session = curl_init($solicitarUrlFile->uploadUrl);//Provided by b2_get_upload_url
+        if (!empty($archivo_subir)) {
 
+            $data["files_id"] = $archivo_subir[0]["id"];
+            $data["estatus_id"] = 3;
+
+            $solicitarUrlFile = $this->solicitarUrlFile();
+
+            $this->subida_php_backblaze_model->inicio_subida_php_backblaze($data);
+
+            $file_name = $archivo_subir[0]["file_name_original"];
+
+            $my_file = "/var/www/html/PlataformaWEB/uploads/" . $file_name;
+            $handle = fopen($my_file, 'r');
+            $read_file = fread($handle, filesize($my_file));
+            $content_type = mime_content_type($my_file);
+            $sha1_of_file_data = sha1_file($my_file);
+
+            $session = curl_init($solicitarUrlFile->uploadUrl); //Provided by b2_get_upload_url
 // Add read file as post field
-        curl_setopt($session, CURLOPT_POSTFIELDS, $read_file);
+            curl_setopt($session, CURLOPT_POSTFIELDS, $read_file);
 
 // Add headers
-        $headers = array();
-        $headers[] = "Authorization: " . $solicitarUrlFile->authorizationToken;//Provided by b2_get_upload_url
-        $headers[] = "X-Bz-File-Name: " . $file_name;
-        $headers[] = "Content-Type: " . $content_type;
-        $headers[] = "X-Bz-Content-Sha1: " . $sha1_of_file_data;
-        
-        curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
+            $headers = array();
+            $headers[] = "Authorization: " . $solicitarUrlFile->authorizationToken; //Provided by b2_get_upload_url
+            $headers[] = "X-Bz-File-Name: " . $file_name;
+            $headers[] = "Content-Type: " . $content_type;
+            $headers[] = "X-Bz-Content-Sha1: " . $sha1_of_file_data;
 
-        curl_setopt($session, CURLOPT_POST, true); // HTTP POST
-        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);  // Receive server response
-        $server_output = curl_exec($session); // Let's do this!
-        curl_close($session); // Clean up
-        echo ($server_output); // Tell me about the rabbits, George!
+            curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
+
+            curl_setopt($session, CURLOPT_POST, true); // HTTP POST
+            curl_setopt($session, CURLOPT_RETURNTRANSFER, true);  // Receive server response
+            $server_output = curl_exec($session); // Let's do this!
+            curl_close($session); // Clean up
+            echo ($server_output); // Tell me about the rabbits, George!
+            
+            $data["estatus_id"] = 4;
+            $this->subida_php_backblaze_model->final_subida_php_backblaze($data);
+        }
     }
 
     public function solicitarUrlFile() {
 
         $datosAutorizacion = $this->autorization();
-        $session = curl_init($datosAutorizacion->apiUrl . $this->config->item('url_upload'));
+        $session = curl_init($datosAutorizacion->apiUrl . $this->config->item('url'
+                . '_upload'));
 
         // Add post fields
         $data = array("bucketId" => $this->config->item('bucket_id')); // The ID of the bucket you want to upload to
@@ -60,7 +75,7 @@ class ServiceUploadController extends CI_Controller {
         $server_output = curl_exec($session); // Let's do this!
         curl_close($session); // Clean up
         header('Content-Type: application/json');
-        
+
         return json_decode($server_output); // Tell me about the rabbits, George!
     }
 
---
+++ application/models/Subida_php_backblaze_model.php
@@ -0,0 +1,184 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Subida_php_backblaze_model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function listar_archivos_a_subir() {
+        $this->db->select('*');
+        $this->db->from('files_estatus');
+        $this->db->join('files', 'files.id = files_estatus.files_id');
+        $this->db->where('estatus_id', 2);
+        $this->db->where('files.deleted', 0);
+        $this->db->order_by('files.date_created', 'DESC');
+        $this->db->limit(1);
+        $query = $this->db->get();
+//SELECT * FROM space_plataforma.files_estatus
+//JOIN space_plataforma.files ON files_estatus.files_id=files.id
+//WHERE estatus_id=2
+//ORDER BY files.date_created DESC
+//LIMIT 1
+
+        if ($query->num_rows() >= 1) {
+            return $query->result_array();
+        }
+    }
+
+    public function ver_articulo($id) {
+        $this->db->select('*');
+        $this->db->from('articles');
+        $this->db->where('id', $id);
+
+        $query = $this->db->get();
+        if ($query->num_rows() >= 1) {
+            return $query->result_array();
+        }
+    }
+
+    public function inicio_subida_php_backblaze($data) {
+        $this->db->insert('files_estatus', $data);
+    }
+
+    public function final_subida_php_backblaze($data) {
+        $this->db->insert('files_estatus', $data);
+    }
+
+    public function editar_articulo($data) {
+//        $data['date_added'] = date('Y-m-d H:i:s');
+        $data['published'] = 'no';
+
+        if ($data['optionsRadios'] == 'Copita Menstrual') {
+            $data['link1_titulo'] = 'Copita Menstrual';
+            $data['link1_link'] = 'https://www.copitamenstrual.com';
+            $data['link2_titulo'] = 'https://www.copitamenstrual.com';
+            $data['link2_link'] = null;
+        } else {
+            $data['link1_titulo'] = $data['nombre_autor'];
+            $data['link1_link'] = $data['titulo_autor'];
+            $data['link2_titulo'] = $data['url_autor'];
+            $data['link2_link'] = null;
+        }
+        unset($data['optionsRadios']);
+        unset($data['nombre_autor']);
+        unset($data['titulo_autor']);
+        unset($data['url_autor']);
+        unset($data['contenido_hidden']);
+
+        $this->db->where('id', $data['id']);
+        $this->db->update('articles', $data);
+    }
+
+    public function eliminar_articulo($id) {
+        $this->db->delete('articles', array('id' => $id));
+    }
+
+    public function publicar_articulo($id) {
+//        $data['date_added'] = date('Y-m-d H:i:s');
+        $data['published'] = 'yes';
+        $this->db->where('id', $id);
+        $this->db->update('articles', $data);
+    }
+
+    public function despublicar_articulo($id) {
+//        $data['date_added'] = date('Y-m-d H:i:s');
+        $data['published'] = 'no';
+        $this->db->where('id', $id);
+        $this->db->update('articles', $data);
+    }
+
+    public function listar_articulo($row = 0, $row_pages = 6, $categoria = null) {
+        $this->db->select('id , image , desc , slug , date_added , title, link1_titulo, link1_link, link2_titulo, link2_link');
+        $this->db->where('published', 'yes');
+        if ($categoria != null) {
+            $this->db->where('categoria', $categoria);
+        }
+        $this->db->order_by('id', 'desc');
+        $query = $this->db->get('articles', $row_pages, $row);
+        if ($query->num_rows() >= 1) {
+            return $query->result_array();
+        }
+    }
+
+    public function contar_articulos($categoria = null) {
+        $this->db->where('published', 'yes');
+        if ($categoria != null) {
+            $this->db->where('categoria', $categoria);
+        }
+        $query = $this->db->get('articles');
+        if ($query->num_rows() >= 1) {
+            return $query->result_array();
+        }
+    }
+
+    public function registrar_visita($slug, $ip_address) {
+        $this->db->select('*');
+        $this->db->from('articles');
+        $this->db->where('slug', $slug);
+        $this->db->where('published', 'yes');
+        $query = $this->db->get();
+        $articulo_atributo = $query->result_array();
+        $id_articulo = $articulo_atributo[0]['id'];
+        $data = array(
+            'article_id' => $id_articulo,
+            'fecha' => date('Y-m-d'),
+            'ip' => $ip_address
+        );
+        $this->db->insert('articuloslog', $data);
+    }
+
+    public function subir_foto($nombre_foto, $id_article = null) {
+
+        $data = array(
+            'video' => null,
+            'video_lista' => null,
+            'image' => $nombre_foto,
+        );
+
+        if ($id_article != null) {
+            $this->db->where('id', $id_article);
+        } else {
+            $this->db->select_max('id');
+            $query = $this->db->get('articles');
+            if ($query->num_rows() >= 1) {
+                $id_max = $query->result_array();
+                $this->db->where('id', $id_max[0]['id']);
+            }
+        }
+
+        $this->db->update('articles', $data);
+    }
+
+    public function categoria_articulo() {
+        $this->db->distinct();
+        $this->db->select('categoria');
+        $this->db->from('articles');
+        $query = $this->db->get();
+        if ($query->num_rows() >= 1) {
+            return $query->result_array();
+        }
+    }
+
+    public function subir_video($data, $id = null) {
+        $this->db->where('id', $id);
+        $this->db->update('articles', $data);
+    }
+
+    public function list_all($categoria = null) {
+        $this->db->select('*');
+//        $this->db->where('published', 'yes');
+        if ($categoria != null) {
+            $this->db->where('categoria', $categoria);
+        }
+        $this->db->order_by('id', 'desc');
+        $query = $this->db->get('articles');
+        if ($query->num_rows() >= 1) {
+            return $query->result_array();
+        }
+    }
+
+}
